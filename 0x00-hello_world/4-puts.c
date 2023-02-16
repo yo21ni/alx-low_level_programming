16c6204e55fe123902e96d25cc306f160e096b7f
yo21ni <yonatanbirhanu221@gmail.com>
2023-02-16 15:53:10 -0800

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle. followed by a new line."
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	put("/"Programming is like building a multilingual puzzle"");
+    return (0);
+}
